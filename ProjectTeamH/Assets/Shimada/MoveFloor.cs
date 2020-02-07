37a062ae495e1e35c8b6ab2311c5838b0603aad1
shimada15 <iukfsafczn@i.softbank.jp>
2020-02-07 13:15:03 +0900

    左右に動く床の作成
    床作成
    左右に動く床の作成
    動く床のスイッチ作成

---
+++ ProjectTeamH/Assets/Shimada/MoveFloor.cs
@@ -0,0 +1,32 @@
+﻿//2020/2/7
+//移動する床
+//島田一宏
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveFloor : MonoBehaviour
+{
+    //変数定義
+    private Rigidbody2D rb;
+    private Vector2 defaultpass;
+
+    void Start()
+    {
+        FlagManager.Instance.ResetFlags();
+        rb = GetComponent<Rigidbody2D>();
+        defaultpass = transform.position;
+        
+    }
+
+    void Update()
+    {
+        Time.timeScale = 0.0f;
+        if (FlagManager.Instance.flags[0] == true)
+        {
+            Time.timeScale = 1.0f;
+            //X座標のみ横移動
+            rb.transform.position =new Vector2(defaultpass.x + Mathf.PingPong(Time.time, 3), defaultpass.y);
+        }
+    }
+}
---
+++ ProjectTeamH/Assets/Shimada/MoveSwitch.cs
@@ -0,0 +1,29 @@
+﻿//2020/2/7
+//動く床のスイッチ
+//島田一宏
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveSwitch : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (FlagManager.Instance.flags[0] == false)
+            {
+                FlagManager.Instance.flags[0] = true;
+            }
+            else if (FlagManager.Instance.flags[0] == true)
+            {
+                FlagManager.Instance.flags[0] = false;
+            }
+        }
+    }
+}

435028a4a99f76b054f68694f995c1ab7507b18e
原国政之 <haraguni001@gmail.com>
2020-04-11 08:11:15 +0900

    バケツの挙動完成（仮）
    今の所、スペースキーでバケツ使用になってます。
    酸のタグ(Acid)のオブジェクトに触れると、そのオブジェクトを削除して酸が入った状態に、もう一度使うと、酸を生成して酸がない状態に戻ります。
    コライダーがそのままだと、酸を零した後すぐ汲む形になってしまったので、BoxCollider2Dをオンオフしています。
    でも、Colliderをオンにした後OnTriggerEnter2Dが反応しないので、原因を探ってます。
    Transformを少しでもいじるとOnTriggerEnter2Dが呼ばれたので、角度を微妙にズラして無理やり呼び起こして動作させています。

--- ProjectTeamH/Assets/Haraguni/Scripts/Baketsu.cs
+++ ProjectTeamH/Assets/Haraguni/Scripts/Baketsu.cs
@@ -10,63 +10,67 @@ public class Baketsu : MonoBehaviour
     //変更するスプライト
     [SerializeField]
     private Sprite[] spr=new Sprite[2];
-    private bool isMax;
-    private bool isUse;
+    private bool isMax; //酸を汲んだか判定
     //バケツから出す酸
     [SerializeField]
     private GameObject acid;
     private float hp; //耐久値
-    private float pumpTime;
+    private float useTime; //汲む時間・零す時間
+    //汲み終わる時間・零し終わる時間
+    [SerializeField]
+    private float useEndTime;
     void Start()
     {
         isMax = false;
-        isUse = false;
-        pumpTime = 0.0f;
+        useTime = 0.0f;
         GetComponent<BoxCollider2D>().enabled = false;
         this.gameObject.SetActive(false);
     }
  
     void Update()
     {
-            
-        Pump();
+            if(!isMax) Pump();
+            if(isMax) Spill();
     }
 
-    void Spill()
-    {
-            renderer.sprite = spr[0];
-
-        GameObject A = Instantiate(acid) as GameObject;
-        A.transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
-
-        isMax = false;
-        GetComponent<BoxCollider2D>().enabled = false;
-        this.gameObject.SetActive(false);
-    }
+    //酸を汲む処理
     void Pump()
     {
-        //酸を汲む時
-        if(!isMax)
-        {
-            pumpTime += Time.deltaTime;
-                if (pumpTime>=1.0f)
+            useTime += Time.deltaTime;
+                if (useTime>= useEndTime)
                 {
                 GetComponent<BoxCollider2D>().enabled = true;
-                pumpTime = 0.0f;
+            transform.Rotate(0, 1, 0);
+                useTime = 0.0f;
             }
-        }
-        //酸を零す時
-        else if(isMax)
+    }
+
+    //酸を零す処理
+    void Spill()
+    {
+        useTime += Time.deltaTime;
+        if (useTime >= useEndTime)
         {
             GetComponent<BoxCollider2D>().enabled = false;
-            pumpTime += Time.deltaTime;
-            if (pumpTime>=1.0f)
-                {
-                GetComponent<BoxCollider2D>().enabled = true;
-                pumpTime = 0.0f;
-                Spill();
-            }
+            useTime = 0.0f;
+
+        //汲む前の見た目に戻す
+        renderer.sprite = spr[0];
+        //酸をバケツの位置に生成する
+        GameObject A = Instantiate(acid) as GameObject;
+        A.transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+        //汲んでない状態にしてオブジェクトを隠す
+        isMax = false;
+            transform.Rotate(0, -1, 0);
+        this.gameObject.SetActive(false);
+
         }
+    }
+
+    //耐久値が減る処理
+    void MinusHealth()
+    {
+
     }
 
     void OnTriggerEnter2D(Collider2D col)
@@ -79,6 +83,8 @@ public class Baketsu : MonoBehaviour
             renderer.sprite = spr[1];
             this.gameObject.SetActive(false);
         }
+
+        //酸以外のオブジェクトに触れた場合、そのまま戻す
         else
         {
             renderer.sprite = spr[0];

589db1ee1bf81fefd9d52c81acfeb016acc2320b
KPC-017A2070\K017A2070 <tyu_sonbou@icloud.com>
2020-04-13 00:17:30 +0900

    ベルトコンベア作成
    オブジェクトが自動で動く床作成
    左右切り替えのスイッチはまだ未作

---
+++ ProjectTeamH/Assets/Nakamura/Script/BeltConveyor.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeltConveyor : MonoBehaviour
+{
+    [SerializeField]
+    float speed;
+    [SerializeField]
+    /*static*/ bool LRSwich;
+
+    float LR;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        LR = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(LRSwich)
+        {
+            LR = -1;
+        }
+        else
+        {
+            LR = 1;
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D col)
+    {
+        col.gameObject.transform.position += new Vector3(speed * Time.deltaTime * LR, 0, 0);
+    }
+
+    //public static bool ChangeLR()
+    //{
+    //    return LRSwich;
+    //}
+}

b0e779f8d9ee08b539d957d9ef9128c0d2e74f39
KPC-017A2070\K017A2070 <tyu_sonbou@icloud.com>
2020-04-15 23:38:23 +0900

    StageSelectシーン完成
    ページ遷移できるように
    現状、8の倍数ぴったりでしか表示できないので
    ステージ数が8の倍数以外になった時に要修正

--- ProjectTeamH/Assets/Nakamura/Script/StageSelectScript.cs
+++ ProjectTeamH/Assets/Nakamura/Script/StageSelectScript.cs
@@ -7,16 +7,24 @@ using UnityEngine.SceneManagement;
 public class StageSelectScript : MonoBehaviour
 {
     [SerializeField]
-    private GameObject[] gameObjects;
+    private GameObject[] Pages; //ページ配列
 
     [SerializeField]
-    private string[] SceneName;
+    private GameObject[] Stages; //ステージ配列
+
+    [SerializeField]
+    private string[] SceneName; //シーン名配列
 
     // 大きさをInspector上で自由に設定できるようにする
     public Vector3 scale;
+    //ページ遷移スピード
+    public float PageSpeed;
 
-    string PageState;
-    string StageState;
+    public GameObject NextPage;
+    public GameObject BackPage;
+
+    string PageState; //何ページ目か
+    string StageState; //どのステージを選択しているか
 
     // Start is called before the first frame update
     void Start()
@@ -33,6 +41,7 @@ public class StageSelectScript : MonoBehaviour
         StageChoice();
     }
 
+    //ステージ決定
     private void StageChoice()
     {
         if (Input.GetKeyDown(KeyCode.Space))
@@ -84,42 +93,218 @@ public class StageSelectScript : MonoBehaviour
                     }
 
                     break;
+
+                case "PAGE2":
+                    switch (StageState)
+                    {
+                        case "STAGE1":
+                            SceneManager.LoadScene(SceneName[9]);
+
+                            break;
+
+                        case "STAGE2":
+                            SceneManager.LoadScene(SceneName[10]);
+
+                            break;
+
+                        case "STAGE3":
+                            SceneManager.LoadScene(SceneName[11]);
+
+                            break;
+
+                        case "STAGE4":
+                            SceneManager.LoadScene(SceneName[12]);
+
+                            break;
+
+                        case "STAGE5":
+                            SceneManager.LoadScene(SceneName[13]);
+
+                            break;
+
+                        case "STAGE6":
+                            SceneManager.LoadScene(SceneName[14]);
+
+                            break;
+
+                        case "STAGE7":
+                            SceneManager.LoadScene(SceneName[15]);
+
+                            break;
+
+                        case "STAGE8":
+                            SceneManager.LoadScene(SceneName[16]);
+
+                            break;
+                    }
+
+                    break;
+
+                case "PAGE3":
+                    switch (StageState)
+                    {
+                        case "STAGE1":
+                            SceneManager.LoadScene(SceneName[17]);
+
+                            break;
+
+                        case "STAGE2":
+                            SceneManager.LoadScene(SceneName[18]);
+
+                            break;
+
+                        case "STAGE3":
+                            SceneManager.LoadScene(SceneName[19]);
+
+                            break;
+
+                        case "STAGE4":
+                            SceneManager.LoadScene(SceneName[20]);
+
+                            break;
+
+                        case "STAGE5":
+                            SceneManager.LoadScene(SceneName[21]);
+
+                            break;
+
+                        case "STAGE6":
+                            SceneManager.LoadScene(SceneName[22]);
+
+                            break;
+
+                        case "STAGE7":
+                            SceneManager.LoadScene(SceneName[23]);
+
+                            break;
+
+                        case "STAGE8":
+                            SceneManager.LoadScene(SceneName[24]);
+
+                            break;
+                    }
+
+                    break;
             }
         }
         
     }
 
+    //ページ変更描画
     private void PageChange()
     {
+        RectTransform P1Pos = Pages[1].GetComponent<RectTransform>();
+        RectTransform P2Pos = Pages[2].GetComponent<RectTransform>();
+        RectTransform P3Pos = Pages[3].GetComponent<RectTransform>();
+
+
         switch (PageState)
         {
             case "PAGE1":
-                gameObjects[1] = GameObject.Find("StageImage1");
-                gameObjects[2] = GameObject.Find("StageImage2");
-                gameObjects[3] = GameObject.Find("StageImage3");
-                gameObjects[4] = GameObject.Find("StageImage4");
-                gameObjects[5] = GameObject.Find("StageImage5");
-                gameObjects[6] = GameObject.Find("StageImage6");
-                gameObjects[7] = GameObject.Find("StageImage7");
-                gameObjects[8] = GameObject.Find("StageImage8");
+                //ステージ1~8まで格納
+                for(int i = 1; i < Stages.Length; i++)
+                {
+                    Stages[i] = GameObject.Find("StageImage" + i);
+                }
+
+                BackPage.SetActive(false);
+
+                if (P1Pos.localPosition.x != 0)
+                {
+                    P1Pos.localPosition += new Vector3(PageSpeed, 0, 0);
+                    P2Pos.localPosition += new Vector3(PageSpeed, 0, 0);
+                    P3Pos.localPosition += new Vector3(PageSpeed, 0, 0);
+                }
+                
+                if(Input.GetKeyDown(KeyCode.R))
+                {
+                    PageState = "PAGE2";
+                }
+
+                break;
+
+            case "PAGE2":
+                //ステージ9~16まで格納
+                for (int i = 1; i < Stages.Length; i++)
+                {
+                    Stages[i] = GameObject.Find("StageImage" + (i + 8));
+                }
+
+                BackPage.SetActive(true);
+                NextPage.SetActive(true);
+
+                if (P2Pos.localPosition.x != 0 && P2Pos.localPosition.x > 0)
+                {
+                    P1Pos.localPosition -= new Vector3(PageSpeed, 0, 0);
+                    P2Pos.localPosition -= new Vector3(PageSpeed, 0, 0);
+                    P3Pos.localPosition -= new Vector3(PageSpeed, 0, 0);
+                }
+                if (P2Pos.localPosition.x != 0 && P2Pos.localPosition.x < 0)
+                {
+                    P1Pos.localPosition += new Vector3(PageSpeed, 0, 0);
+                    P2Pos.localPosition += new Vector3(PageSpeed, 0, 0);
+                    P3Pos.localPosition += new Vector3(PageSpeed, 0, 0);
+                }
+
+                if (Input.GetKeyDown(KeyCode.L))
+                {
+                    PageState = "PAGE1";
+                }
+
+                if (Input.GetKeyDown(KeyCode.R))
+                {
+                    PageState = "PAGE3";
+                }
+
+                break;
+
+            case "PAGE3":
+                //ステージ17~24まで格納
+                for (int i = 1; i < Stages.Length; i++)
+                {
+                    Stages[i] = GameObject.Find("StageImage" + (i + 16));
+                }
+
+                NextPage.SetActive(false);
+
+                if (P3Pos.localPosition.x != 0)
+                {
+                    P1Pos.localPosition -= new Vector3(PageSpeed, 0, 0);
+                    P2Pos.localPosition -= new Vector3(PageSpeed, 0, 0);
+                    P3Pos.localPosition -= new Vector3(PageSpeed, 0, 0);
+                }
+
+                if (Input.GetKeyDown(KeyCode.L))
+                {
+                    PageState = "PAGE2";
+                }
 
                 break;
         }
     }
 
+
+    //ステージ選択描画
     private void StageSelect()
     {
+        //ページ変更時にSTAGE1状態にする
+        if(Input.GetKeyDown(KeyCode.R) || Input.GetKeyDown(KeyCode.L))
+        {
+            StageState = "STAGE1";
+        }
+
+
         switch (StageState)
         {
             case "STAGE1":
-                gameObjects[1].transform.localScale = new Vector3(scale.x, scale.y, scale.z);
-                gameObjects[2].transform.localScale = new Vector3(1, 1, 1);
-                gameObjects[3].transform.localScale = new Vector3(1, 1, 1);
-                gameObjects[4].transform.localScale = new Vector3(1, 1, 1);
-                gameObjects[5].transform.localScale = new Vector3(1, 1, 1);
-                gameObjects[6].transform.localScale = new Vector3(1, 1, 1);
-                gameObjects[7].transform.localScale = new Vector3(1, 1, 1);
-                gameObjects[8].transform.localScale = new Vector3(1, 1, 1);
+                Stages[1].transform.localScale = new Vector3(scale.x, scale.y, scale.z);
+                Stages[2].transform.localScale = new Vector3(1, 1, 1);
+                Stages[3].transform.localScale = new Vector3(1, 1, 1);
+                Stages[4].transform.localScale = new Vector3(1, 1, 1);
+                Stages[5].transform.localScale = new Vector3(1, 1, 1);
+                Stages[6].transform.localScale = new Vector3(1, 1, 1);
+                Stages[7].transform.localScale = new Vector3(1, 1, 1);
+                Stages[8].transform.localScale = new Vector3(1, 1, 1);
 
                 if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
                 {
@@ -133,14 +318,14 @@ public class StageSelectScript : MonoBehaviour
                 break;
 
             case "STAGE2":
-                gameObjects[1].transform.localScale = new Vector3(1, 1, 1);
-                gameObjects[2].transform.localScale = new Vector3(scale.x, scale.y, scale.z);
-                gameObjects[3].transform.localScale = new Vector3(1, 1, 1);
-                gameObjects[4].transform.localScale = new Vector3(1, 1, 1);
-                gameObjects[5].transform.localScale = new Vector3(1, 1, 1);
-                gameObjects[6].transform.localScale = new Vector3(1, 1, 1);
-                gameObjects[7].transform.localScale = new Vector3(1, 1, 1);
-                gameObjects[8].transform.localScale = new Vector3(1, 1, 1);
+                Stages[1].transform.localScale = new Vector3(1, 1, 1);
+                Stages[2].transform.localScale = new Vector3(scale.x, scale.y, scale.z);
+                Stages[3].transform.localScale = new Vector3(1, 1, 1);
+                Stages[4].transform.localScale = new Vector3(1, 1, 1);
+                Stages[5].transform.localScale = new Vector3(1, 1, 1);
+                Stages[6].transform.localScale = new Vector3(1, 1, 1);
+                Stages[7].transform.localScale = new Vector3(1, 1, 1);
+                Stages[8].transform.localScale = new Vector3(1, 1, 1);
 
                 if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
                 {
@@ -158,14 +343,14 @@ public class StageSelectScript : MonoBehaviour
                 break;
 
             case "STAGE3":
-                gameObjects[1].transform.localScale = new Vector3(1, 1, 1);
-                gameObjects[2].transform.localScale = new Vector3(1, 1, 1);
-                gameObjects[3].transform.localScale = new Vector3(scale.x, scale.y, scale.z);
-                gameObjects[4].transform.localScale = new Vector3(1, 1, 1);
-                gameObjects[5].transform.localScale = new Vector3(1, 1, 1);
-                gameObjects[6].transform.localScale = new Vector3(1, 1, 1);
-                gameObjects[7].transform.localScale = new Vector3(1, 1, 1);
-                gameObjects[8].transform.localScale = new Vector3(1, 1, 1);
+                Stages[1].transform.localScale = new Vector3(1, 1, 1);
+                Stages[2].transform.localScale = new Vector3(1, 1, 1);
+                Stages[3].transform.localScale = new Vector3(scale.x, scale.y, scale.z);
+                Stages[4].transform.localScale = new Vector3(1, 1, 1);
+                Stages[5].transform.localScale = new Vector3(1, 1, 1);
+                Stages[6].transform.localScale = new Vector3(1, 1, 1);
+                Stages[7].transform.localScale = new Vector3(1, 1, 1);
+                Stages[8].transform.localScale = new Vector3(1, 1, 1);
 
                 if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
                 {
@@ -183,14 +368,14 @@ public class StageSelectScript : MonoBehaviour
                 break;
 
             case "STAGE4":
-                gameObjects[1].transform.localScale = new Vector3(1, 1, 1);
-                gameObjects[2].transform.localScale = new Vector3(1, 1, 1);
-                gameObjects[3].transform.localScale = new Vector3(1, 1, 1);
-                gameObjects[4].transform.localScale = new Vector3(scale.x, scale.y, scale.z);
-                gameObjects[5].transform.localScale = new Vector3(1, 1, 1);
-                gameObjects[6].transform.localScale = new Vector3(1, 1, 1);
-                gameObjects[7].transform.localScale = new Vector3(1, 1, 1);
-                gameObjects[8].transform.localScale = new Vector3(1, 1, 1);
+                Stages[1].transform.localScale = new Vector3(1, 1, 1);
+                Stages[2].transform.localScale = new Vector3(1, 1, 1);
+                Stages[3].transform.localScale = new Vector3(1, 1, 1);
+                Stages[4].transform.localScale = new Vector3(scale.x, scale.y, scale.z);
+                Stages[5].transform.localScale = new Vector3(1, 1, 1);
+                Stages[6].transform.localScale = new Vector3(1, 1, 1);
+                Stages[7].transform.localScale = new Vector3(1, 1, 1);
+                Stages[8].transform.localScale = new Vector3(1, 1, 1);
 
                 if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
                 {
@@ -204,14 +389,14 @@ public class StageSelectScript : MonoBehaviour
                 break;
 
             case "STAGE5":
-                gameObjects[1].transform.localScale = new Vector3(1, 1, 1);
-                gameObjects[2].transform.localScale = new Vector3(1, 1, 1);
-                gameObjects[3].transform.localScale = new Vector3(1, 1, 1);
-                gameObjects[4].transform.localScale = new Vector3(1, 1, 1);
-                gameObjects[5].transform.localScale = new Vector3(scale.x, scale.y, scale.z);
-                gameObjects[6].transform.localScale = new Vector3(1, 1, 1);
-                gameObjects[7].transform.localScale = new Vector3(1, 1, 1);
-                gameObjects[8].transform.localScale = new Vector3(1, 1, 1);
+                Stages[1].transform.localScale = new Vector3(1, 1, 1);
+                Stages[2].transform.localScale = new Vector3(1, 1, 1);
+                Stages[3].transform.localScale = new Vector3(1, 1, 1);
+                Stages[4].transform.localScale = new Vector3(1, 1, 1);
+                Stages[5].transform.localScale = new Vector3(scale.x, scale.y, scale.z);
+                Stages[6].transform.localScale = new Vector3(1, 1, 1);
+                Stages[7].transform.localScale = new Vector3(1, 1, 1);
+                Stages[8].transform.localScale = new Vector3(1, 1, 1);
 
                 if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
                 {
@@ -225,14 +410,14 @@ public class StageSelectScript : MonoBehaviour
                 break;
 
             case "STAGE6":
-                gameObjects[1].transform.localScale = new Vector3(1, 1, 1);
-                gameObjects[2].transform.localScale = new Vector3(1, 1, 1);
-                gameObjects[3].transform.localScale = new Vector3(1, 1, 1);
-                gameObjects[4].transform.localScale = new Vector3(1, 1, 1);
-                gameObjects[5].transform.localScale = new Vector3(1, 1, 1);
-                gameObjects[6].transform.localScale = new Vector3(scale.x, scale.y, scale.z);
-                gameObjects[7].transform.localScale = new Vector3(1, 1, 1);
-                gameObjects[8].transform.localScale = new Vector3(1, 1, 1);
+                Stages[1].transform.localScale = new Vector3(1, 1, 1);
+                Stages[2].transform.localScale = new Vector3(1, 1, 1);
+                Stages[3].transform.localScale = new Vector3(1, 1, 1);
+                Stages[4].transform.localScale = new Vector3(1, 1, 1);
+                Stages[5].transform.localScale = new Vector3(1, 1, 1);
+                Stages[6].transform.localScale = new Vector3(scale.x, scale.y, scale.z);
+                Stages[7].transform.localScale = new Vector3(1, 1, 1);
+                Stages[8].transform.localScale = new Vector3(1, 1, 1);
 
                 if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
                 {
@@ -250,14 +435,14 @@ public class StageSelectScript : MonoBehaviour
                 break;
 
             case "STAGE7":
-                gameObjects[1].transform.localScale = new Vector3(1, 1, 1);
-                gameObjects[2].transform.localScale = new Vector3(1, 1, 1);
-                gameObjects[3].transform.localScale = new Vector3(1, 1, 1);
-                gameObjects[4].transform.localScale = new Vector3(1, 1, 1);
-                gameObjects[5].transform.localScale = new Vector3(1, 1, 1);
-                gameObjects[6].transform.localScale = new Vector3(1, 1, 1);
-                gameObjects[7].transform.localScale = new Vector3(scale.x, scale.y, scale.z);
-                gameObjects[8].transform.localScale = new Vector3(1, 1, 1);
+                Stages[1].transform.localScale = new Vector3(1, 1, 1);
+                Stages[2].transform.localScale = new Vector3(1, 1, 1);
+                Stages[3].transform.localScale = new Vector3(1, 1, 1);
+                Stages[4].transform.localScale = new Vector3(1, 1, 1);
+                Stages[5].transform.localScale = new Vector3(1, 1, 1);
+                Stages[6].transform.localScale = new Vector3(1, 1, 1);
+                Stages[7].transform.localScale = new Vector3(scale.x, scale.y, scale.z);
+                Stages[8].transform.localScale = new Vector3(1, 1, 1);
 
                 if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
                 {
@@ -275,14 +460,14 @@ public class StageSelectScript : MonoBehaviour
                 break;
 
             case "STAGE8":
-                gameObjects[1].transform.localScale = new Vector3(1, 1, 1);
-                gameObjects[2].transform.localScale = new Vector3(1, 1, 1);
-                gameObjects[3].transform.localScale = new Vector3(1, 1, 1);
-                gameObjects[4].transform.localScale = new Vector3(1, 1, 1);
-                gameObjects[5].transform.localScale = new Vector3(1, 1, 1);
-                gameObjects[6].transform.localScale = new Vector3(1, 1, 1);
-                gameObjects[7].transform.localScale = new Vector3(1, 1, 1);
-                gameObjects[8].transform.localScale = new Vector3(scale.x, scale.y, scale.z);
+                Stages[1].transform.localScale = new Vector3(1, 1, 1);
+                Stages[2].transform.localScale = new Vector3(1, 1, 1);
+                Stages[3].transform.localScale = new Vector3(1, 1, 1);
+                Stages[4].transform.localScale = new Vector3(1, 1, 1);
+                Stages[5].transform.localScale = new Vector3(1, 1, 1);
+                Stages[6].transform.localScale = new Vector3(1, 1, 1);
+                Stages[7].transform.localScale = new Vector3(1, 1, 1);
+                Stages[8].transform.localScale = new Vector3(scale.x, scale.y, scale.z);
 
                 if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
                 {

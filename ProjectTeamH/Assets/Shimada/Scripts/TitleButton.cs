5673e22c82ff5c3fffbb1bf9786d2fb0ced32a04
shimada15 <iukfsafczn@i.softbank.jp>
2020-04-17 01:20:27 +0900

    シーン遷移の追加
    タイトルシーン追加
    エンディングシーン追加
    タイトルとエンディングのシーン遷移

---
+++ ProjectTeamH/Assets/Shimada/Scripts/TitleButton.cs
@@ -0,0 +1,28 @@
+﻿//2020/04/17
+//タイトルシーンのボタン
+//島田一宏
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void toGameScene()
+    {
+
+        FadeManager.Instance.LoadScene("Ending", 2.0f);
+    }
+}

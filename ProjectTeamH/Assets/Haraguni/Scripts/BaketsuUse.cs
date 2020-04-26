15308a912e750dcb912711a56c35b068c5f9a703
原国政之 <haraguni001@gmail.com>
2020-04-27 02:58:24 +0900

    バケツを一部変更しました
    耐久値がなくなったら酸を出して、使えなくなるようにしました。
    recoveryTimeが0になったらもう一度使えるようになります。

--- ProjectTeamH/Assets/Haraguni/Scripts/BaketsuUse.cs
+++ ProjectTeamH/Assets/Haraguni/Scripts/BaketsuUse.cs
@@ -4,22 +4,35 @@ using UnityEngine;
 
 public class BaketsuUse : MonoBehaviour
 {
+    [SerializeField]
     private GameObject baketsu;
+    [SerializeField]
     private Baketsu bake;
-    void Start()
-    {
-        //使うバケツを取得する
-        baketsu = gameObject.transform.Find("baketsu").gameObject;
-        bake = baketsu.GetComponent<Baketsu>();
-    }
+    //バケツが壊れた時一度だけバケツを呼び出す（酸を出す）
+    private bool isBreak;
 
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
+        if(bake.hp<=0)
+        {
+            bake.HpRecovery();
+            if (!isBreak)
+            {
+                baketsu.SetActive(true);
+                isBreak = true;
+            }
+        }
+        else
+        {
+            isBreak = false;
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space) && bake.hp>=0)
         {
             //バケツを表示する
             baketsu.SetActive(true);
         }
+
 
         if(bake.IsMax())
         {

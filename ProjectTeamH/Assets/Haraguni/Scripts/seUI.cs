e9249d2691889b3407d9cc5ab55f0552dded4c94
原国政之 <haraguni001@gmail.com>
2020-05-02 23:41:32 +0900

    ＳＥのスライダーの処理と、スライダー選択処理追加
    ＳＥのスライダーを動かす処理と、コントローラーのスティック上下でＢＧＭとＳＥとスライダー選択を変える処理を追加しました。
    ただ、スティックを左右に動かすとすぐ１か０になったり、
    上下左右推しても反応しない事があるので、調整します。

---
+++ ProjectTeamH/Assets/Haraguni/Scripts/seUI.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class seUI : MonoBehaviour
+{
+    private Slider slider;
+    private float volume;
+    private float moveCursorX;
+    public bool isSelect;
+    void Start()
+    {
+        slider = GetComponent<Slider>();
+        volume = slider.value;
+        isSelect = false;
+        moveCursorX = 0.0f;
+    }
+
+    void Update()
+    {
+        if (!isSelect) return;
+
+        if (Input.GetAxisRaw("Vertical")<0)
+        {
+            isSelect = false;
+            return;
+        }
+        MoveSlider();
+
+    }
+
+    public void MoveSlider()
+    {
+        moveCursorX += Input.GetAxis("Horizontal")*0.1f;
+        slider.value = moveCursorX;
+        volume = slider.value;
+        Debug.Log("SEMOVING" + volume);
+    }
+
+    public bool IsSelect()
+    {
+        return isSelect;
+    }
+
+    public bool Select()
+    {
+        return isSelect = true;
+    }
+    public bool NotSelect()
+    {
+        return isSelect = false;
+    }
+}

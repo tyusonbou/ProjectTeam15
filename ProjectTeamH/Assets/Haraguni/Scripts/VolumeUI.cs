4d61ca87e2ed914f1f35aa1850d42429c5d6e0c6
原国政之 <haraguni001@gmail.com>
2020-05-25 11:37:15 +0900

    音量調整完成
    soundStrで入力した文字列に、音量調整した値を保存して再起動後も同じ音量になるようにしています。
    Inspectorから変えれるので、ＢＧＭもＳＥも別の音量に調整出来るようにしました。

--- ProjectTeamH/Assets/Haraguni/Scripts/VolumeUI.cs
+++ ProjectTeamH/Assets/Haraguni/Scripts/VolumeUI.cs
@@ -2,20 +2,44 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Audio;
 
 public class VolumeUI : MonoBehaviour
 {
     [SerializeField]
     private Slider slider;
-    private float volume;
+    public float soundVolume;
     public bool isSelect;
     [SerializeField]
     private Image handle;
+
+    [SerializeField]
+    private AudioSource audio;
+    [SerializeField]
+    private AudioMixer mixer;
+
+    //ＢＧＭかＳＥか
+    [SerializeField]
+    private bool isBGM;
+
+    //音量を保存する。
+    [SerializeField]
+    private string soundStr;
+    private float prefsVolume;
     void Start()
     {
-        volume = 100;
-        slider.value = 100;
+        soundVolume = PlayerPrefs.GetFloat(soundStr,0);
+        slider.value = soundVolume;
         handle.color = new Color(1.0f, 1.0f, 1.0f, 1.0f);
+        if (isBGM)
+        {
+            mixer.SetFloat("BGM", slider.value);
+        }
+
+        else
+        {
+            mixer.SetFloat("SE", slider.value);
+        }
     }
 
     void Update()
@@ -28,25 +52,43 @@ public class VolumeUI : MonoBehaviour
         if (!isSelect) {
             handle.color = new Color(1.0f, 1.0f, 1.0f, 1.0f);
             return; }
-        if (volume <= 0) volume = 0;
-        if (volume >= 100) volume = 100;
-        slider.value = volume;
+        if (soundVolume <= -80) soundVolume = -80;
+        if (soundVolume >= 20) soundVolume = 20;
+        slider.value = soundVolume;
         if (Input.GetAxis("Horizontal") > 0)
         {
-            volume += 1;
+            soundVolume += 1;
         }
         if (Input.GetAxis("Horizontal") < 0)
         {
-            volume -= 1;
+            soundVolume -= 1;
         }
-        slider.value = volume;
+        slider.value = soundVolume;
+        PlayerPrefs.SetFloat(soundStr, soundVolume);
+        PlayerPrefs.Save();
+        if (isBGM)
+        {
+            mixer.SetFloat("BGM", slider.value);
+        }
+        else
+        {
+            mixer.SetFloat("SE", slider.value);
+        }
+
+        //if (isBGM) mixer.SetFloat("bgm", Mathf.Clamp(volumeDB, -80.0f, 0.0f));
+        //else mixer.SetFloat("se", Mathf.Clamp(volumeDB, -80.0f, 0.0f));
+
+        //if (isBgm) mixer.SetFloat("bgm", mixVor);
+        //else mixer.SetFloat("se", mixVor);
+        //float ConvertVolume2dB(float volume) => 20f * Mathf.Log10(Mathf.Clamp(volume, 0f, 1f));
+        //audio.volume = slider.value / 100;
     }
 
+    //選択中かどうか（選択中は調整できる）
     public bool IsSelect()
     {
         return isSelect;
     }
-
     public bool Select()
     {
         return isSelect = true;
@@ -54,5 +96,11 @@ public class VolumeUI : MonoBehaviour
     public bool NotSelect()
     {
         return isSelect = false;
+    }
+
+    //ボリュームを渡す
+    public float GetVolume()
+    {
+        return soundVolume;
     }
 }

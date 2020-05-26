ad757efd21a9e46f2df2cb2b5494cfe29cf488b2
shimada15 <iukfsafczn@i.softbank.jp>
2020-05-27 01:58:23 +0900

    バルブと溶けるエフェクト追加
    バルブと酸関係の追加
    溶けるエフェクトの追加

---
+++ ProjectTeamH/Assets/Shimada/Scripts/AnimationSqript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationSqript : MonoBehaviour
+{
+    public GameObject obj;
+    Rigidbody2D rb2d;
+    GameObject prefab;
+    private Transform tra;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        tra = this.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Submit"))
+        {
+            StartCoroutine("Animation");
+        }
+    }
+    public void Delete()
+    {
+        Destroy(gameObject);
+    }
+
+    public void Create()
+    {
+        prefab = Instantiate(obj, tra.position, tra.rotation);
+        prefab.transform.parent = transform;
+    }
+
+    private IEnumerator Animation()
+    {
+        transform.Find("溶ける").gameObject.SetActive(true);
+        yield return new WaitForSeconds(0.5f);
+        Create();
+        transform.Find("歯車アイコン11").gameObject.SetActive(false);
+        transform.Find("歯車アイコン12").gameObject.SetActive(false);
+        transform.Find("溶ける").gameObject.SetActive(false);
+
+
+        yield return null;
+    }
+}

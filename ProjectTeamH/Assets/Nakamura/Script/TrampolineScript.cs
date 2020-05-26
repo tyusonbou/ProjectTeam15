872d09997c8abcf25209bb2d99466fd5a99a81d3
KPC-017A2070\K017A2070 <tyu_sonbou@icloud.com>
2020-05-27 02:20:20 +0900

    プレイヤー、トランポリン差し替え。ClearUI追加
    プレイヤーとトランポリンの画像を差し替え
    ステージくりあのUIを追加

--- ProjectTeamH/Assets/Nakamura/Script/TrampolineScript.cs
+++ ProjectTeamH/Assets/Nakamura/Script/TrampolineScript.cs
@@ -7,10 +7,12 @@ public class TrampolineScript : MonoBehaviour
     [SerializeField]
     private float FloatingSpeed;
 
+    Animator animator;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -22,10 +24,12 @@ public class TrampolineScript : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D col)
     {
         col.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * FloatingSpeed);
+        animator.SetTrigger("Spring");
     }
 
     private void OnTriggerEnter2D(Collider2D col)
     {
         col.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * FloatingSpeed);
+        animator.SetTrigger("Spring");
     }
 }
